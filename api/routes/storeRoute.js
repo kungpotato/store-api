ea8e05e0d1217d7492d901eda8b35a55a872d4d2
kungpotato <kungPRS2014>
2018-06-24 18:24:36 +0700

    user update 4

--- api/routes/storeRoute.js
+++ api/routes/storeRoute.js
@@ -1,7 +1,6 @@
 'use strict';
 module.exports = function(app) {
   var Vender = require('../controllers/storeController');
-  var User = require('../controllers/authController');
 
   // Order Routes
   app.route('/orders')
@@ -13,8 +12,4 @@ module.exports = function(app) {
     .get(Vender.read_an_order)
     .put(Vender.update_an_order)
     .delete(Vender.delete_an_order);
-
-  // User Routes
-  app.route('/auths')
-  .get(User.list_all_users);
 };
